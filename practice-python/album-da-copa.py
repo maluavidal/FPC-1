765f3a174265479c778e7231943b3bbad29550f0
Maria Luiza Vidal <marialuizavidal01@gmail.com>
2023-06-19 16:55:08 -0300

    adjusting archives names

--- practice-python/album-da-copa.py
+++
@@ -1,9 +0,0 @@
-total_stickers = int(input())
-bought_stickers = int(input())
-sticker_numbers = []
-
-for _ in bought_stickers:
-    sticker_numbers.append(int(input()))
-
-
-
---
+++ practice-python/album.py
@@ -0,0 +1,9 @@
+total_stickers = int(input())
+bought_stickers = int(input())
+sticker_numbers = []
+
+# for _ in range(bought_stickers):
+#     sticker_numbers.append(int(input()))
+
+print(range(bought_stickers))
+
